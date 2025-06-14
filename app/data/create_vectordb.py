a7a6836c90ddcef38f65086cfc819dd439d8e54e
hqvjet <viethq.1906@gmail.com>
2025-06-14 23:13:15 +0700

    create vectordb creation method

---
+++ app/data/create_vectordb.py
@@ -0,0 +1,6 @@
+from app.data.vector import create_vector_store
+
+if __name__ == "__main__":
+    print("Starting vector store creation...")
+    create_vector_store()
+    print("Vector store created successfully.")
